8d88d70abd58ad0a5f659fdd68d0c2ee5ce2d93f
Nullptr <noreply@nullptr.icu>
2024-07-09 15:58:14 +0800

    Workaround for a bug in libctest

--- user/src/bin/shell.rs
+++ user/src/bin/shell.rs
@@ -3,6 +3,19 @@
 
 use user_lib::syscall::{mount, sys_execve, sys_fork, sys_mkdir, sys_waitpid};
 
+fn run_cmd(cmd: &str) {
+    if sys_fork() == 0 {
+        sys_execve(
+            "/busybox",
+            &["busybox", "sh", "-c", cmd],
+            &["PATH=/:/bin:/lib", "LD_LIBRARY_PATH=/lib"],
+        );
+    } else {
+        let mut result: i32 = 0;
+        sys_waitpid(-1, &mut result);
+    }
+}
+
 #[no_mangle]
 fn main() {
     sys_mkdir("/dev", 0);
@@ -10,14 +23,6 @@ fn main() {
     sys_mkdir("/tmp", 0);
     sys_mkdir("/sys", 0);
     mount("devfs", "/dev", "devfs", 0, None);
-    if sys_fork() == 0 {
-        sys_execve(
-            "/busybox",
-            &["busybox", "sh"],
-            &["PATH=/:/bin:", "LD_LIBRARY_PATH=/lib:", "TERM=screen"],
-        );
-    } else {
-        let mut result: i32 = 0;
-        sys_waitpid(-1, &mut result);
-    }
+    run_cmd("busybox cp /lib/dlopen_dso.so /");
+    run_cmd("busybox sh");
 }
