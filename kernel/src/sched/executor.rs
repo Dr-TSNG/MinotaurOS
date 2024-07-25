b3158c5a9de455a5c001c4fd094a4eb1980e0df6
Nullptr <noreply@nullptr.icu>
2024-07-25 17:21:37 +0800

    Revert "Update scheduler"
    This reverts commit 791e234ee885aeeff52bbe78bf67669ca06ca1af.

--- kernel/src/sched/executor.rs
+++ kernel/src/sched/executor.rs
@@ -1,76 +1,36 @@
 use alloc::collections::VecDeque;
-use core::cell::Cell;
-use async_task::{Builder, ScheduleInfo, Task, WithInfo};
+use async_task::{Runnable, ScheduleInfo, Task, WithInfo};
 use core::future::Future;
-use log::warn;
-use crate::config::MAX_HARTS;
-use crate::driver::BOARD_INFO;
 use crate::process::monitor::MONITORS;
-use crate::processor::hart::local_hart;
 use crate::sync::mutex::IrqMutex;
 
-type Runnable = async_task::Runnable<TaskMeta>;
-
 struct TaskQueue {
-    global_queue: IrqMutex<VecDeque<Runnable>>,
-    hart_queues: [IrqMutex<VecDeque<Runnable>>; MAX_HARTS],
+    queue: IrqMutex<VecDeque<Runnable>>,
 }
-
-#[derive(Default)]
-pub struct TaskMeta {
-    last_hart: Cell<Option<usize>>,
-}
-
-unsafe impl Sync for TaskMeta {}
 
 impl TaskQueue {
     pub const fn new() -> Self {
-        const fn new_mutex() -> IrqMutex<VecDeque<Runnable>> {
-            IrqMutex::new(VecDeque::new())
-        }
         Self {
-            global_queue: new_mutex(),
-            hart_queues: [const { new_mutex() }; MAX_HARTS],
+            queue: IrqMutex::new(VecDeque::new()),
         }
     }
 
     pub fn push_back(&self, runnable: Runnable) {
-        match runnable.metadata().last_hart.get() {
-            Some(hart) => self.hart_queues[hart].lock().push_back(runnable),
-            None => self.global_queue.lock().push_back(runnable),
-        }
+        self.queue.lock().push_back(runnable);
     }
 
     pub fn push_front(&self, runnable: Runnable) {
-        match runnable.metadata().last_hart.get() {
-            Some(hart) => self.hart_queues[hart].lock().push_front(runnable),
-            None => self.global_queue.lock().push_front(runnable),
-        }
+        self.queue.lock().push_front(runnable);
     }
 
     pub fn take(&self) -> Option<Runnable> {
-        fn take_from(queue: &mut VecDeque<Runnable>) -> Option<Runnable> {
-            queue.pop_front()
-        }
-        take_from(&mut self.hart_queues[local_hart().id].lock())
-            .or_else(|| take_from(&mut self.global_queue.lock()))
-            .or_else(|| {
-                for i in 0..BOARD_INFO.smp {
-                    if i != local_hart().id {
-                        if let Some(task) = take_from(&mut self.hart_queues[i].lock()) {
-                            warn!("[executor] Steal task from hart {}", i);
-                            return Some(task);
-                        }
-                    }
-                }
-                None
-            })
+        self.queue.lock().pop_front()
     }
 }
 
 static TASK_QUEUE: TaskQueue = TaskQueue::new();
 
-pub fn spawn<F>(future: F) -> (Runnable, Task<F::Output, TaskMeta>)
+pub fn spawn<F>(future: F) -> (Runnable, Task<F::Output>)
     where
         F: Future + Send + 'static,
         F::Output: Send + 'static,
@@ -82,16 +42,13 @@ pub fn spawn<F>(future: F) -> (Runnable, Task<F::Output, TaskMeta>)
             TASK_QUEUE.push_front(runnable);
         }
     };
-    Builder::new()
-        .metadata(Default::default())
-        .spawn(|_| future, WithInfo(schedule))
+    async_task::spawn(future, WithInfo(schedule))
 }
 
 /// 开始执行任务
 pub fn run_executor() {
     loop {
         if let Some(task) = TASK_QUEUE.take() {
-            task.metadata().last_hart.set(Some(local_hart().id));
             task.run();
         } else {
             core::hint::spin_loop();
